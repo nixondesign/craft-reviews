e2c226c5470cc62a786f3d4f55a27f4bcfbd03e1
Ryan <ryan.pascoe@outlook.com>
2022-06-04 14:17:50 +0100

    Add property annotations to records

--- src/records/Review.php
+++ src/records/Review.php
@@ -4,9 +4,22 @@ namespace rynpsc\reviews\records;
 
 use craft\db\ActiveRecord;
 use craft\elements\User;
+use DateTime;
 use rynpsc\reviews\db\Table;
 use yii\db\ActiveQueryInterface;
 
+/**
+ * @property int $id ID
+ * @property int $elementId Element ID
+ * @property int|null $authorId Author ID
+ * @property int $siteId Site ID
+ * @property int $typeId Type ID
+ * @property string $moderationStatus Moderation status
+ * @property string $review Review
+ * @property int $rating Rating
+ * @property DateTime|null $submissionDate Submission date
+ * @property-read ActiveQueryInterface $user
+ */
 class Review extends ActiveRecord
 {
     /**
--- src/records/ReviewType.php
+++ src/records/ReviewType.php
@@ -5,6 +5,18 @@ namespace rynpsc\reviews\records;
 use craft\db\ActiveRecord;
 use rynpsc\reviews\db\Table;
 
+/**
+ * @property int $id ID
+ * @property string $name Name
+ * @property string $handle Handle
+ * @property int $maxRating Maximum rating
+ * @property string $defaultStatus Default status
+ * @property bool $allowGuestReviews Allow guest reviews
+ * @property bool $requireFullName Require full name
+ * @property int|null $fieldLayoutId Field layout ID
+ * @property bool $hasTitleField Has title field
+ * @property string|null $titleFormat Title format
+ */
 class ReviewType extends ActiveRecord
 {
     /**
