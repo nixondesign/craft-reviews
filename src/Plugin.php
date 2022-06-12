0be2a4d5054fdeb64352a73cdf92d6c3a69e4c05
Ryan <ryan.pascoe@outlook.com>
2022-06-12 22:27:46 +0100

    Rune ECS

--- src/Plugin.php
+++ src/Plugin.php
@@ -237,7 +237,7 @@ class Plugin extends BasePlugin
                         }
 
                         return $controller->resaveElements(Review::class, $criteria);
-                    }
+                    },
                 ];
             }
         );
